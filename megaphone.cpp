aa3183dc04c79f1d112a4293164584e0ddac0ba9
Andreas Maucher <amaucher@c4c2c3.42berlin.de>
2024-02-28 17:16:34 +0100

    t

--- megaphone.cpp
+++ megaphone.cpp
@@ -6,7 +6,27 @@
 /*   By: amaucher <amaucher@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 14:22:27 by amaucher          #+#    #+#             */
-/*   Updated: 2024/02/23 14:22:28 by amaucher         ###   ########.fr       */
+/*   Updated: 2024/02/28 17:16:20 by amaucher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream> //input output
+
+using std::cout; //cout == character out
+
+//ready for portugal
+
+int main(int ac, char **av)
+{
+	int i = 1;
+	int j = 0;
+	
+	if (ac == 1)
+		cout << "LOUD AND UNBEARABLE FEEDBACK NOISE";
+	else
+	{
+		j = 0;
+		for (std::string::iterator it=av[i][j].begin(); it!=av[i][j].end(); ++it; j=0)
+	}
+	return (0);
+}
