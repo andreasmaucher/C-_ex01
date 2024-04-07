912ef893ed1038f3bf6d24f787360cdaac1c0bf6
Andreas Maucher <amaucher@c4a13c1.42berlin.de>
2024-04-07 19:48:17 +0200

    up

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,27 +6,35 @@
 /*   By: amaucher <amaucher@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 14:22:27 by amaucher          #+#    #+#             */
-/*   Updated: 2024/02/28 17:16:20 by amaucher         ###   ########.fr       */
+/*   Updated: 2024/04/07 19:47:33 by amaucher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream> //input output
 
-using std::cout; //cout == character out
+//cout == character out
 
-//ready for portugal
+// std::endl prints a new line character and flushes the output buffer (makes
+// it immediately visible);
+
+// In C++, av is a pointer to an array of strings (char **av), and it should 
+// not be compared directly with a single character like '\0'.
+
+// j starts at -1 to skip the program name
 
 int main(int ac, char **av)
 {
-	int i = 1;
-	int j = 0;
+	int i = 0;
+	int j = -1;
 	
 	if (ac == 1)
-		cout << "LOUD AND UNBEARABLE FEEDBACK NOISE";
-	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	while (ac > 1 && av != nullptr && av[++j])
 	{
-		j = 0;
-		for (std::string::iterator it=av[i][j].begin(); it!=av[i][j].end(); ++it; j=0)
+		i = -1;
+		while (j > 0 && av[j][++i])
+			std::cout << (char)std::toupper(av[j][i]);
 	}
+	std::cout << std::endl;
 	return (0);
 }
